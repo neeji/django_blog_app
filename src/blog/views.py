9383e07cfef59c78d6063621e03a86d2b36d5b62
Neeji <nagg844@gmail.com>
2019-08-03 14:37:37 +0530

    templates work done without admin page and authentication

---
+++ src/blog/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+posts = [
+	{
+		'author' : 'Neeraj',
+		'title' : 'Blog Post 1',
+		'content'  : 'First post content',
+		'date_posted' : 'August 3,2019'
+	},
+	{
+		'author' : 'Aggarwal',
+		'title' : 'Blog Post 2',
+		'content'  : 'First post content',
+		'date_posted' : 'August 3,2019'
+	}
+]
+
+def home(request):
+	context = {
+		'posts' : posts
+	}
+	return render(request,'blog/home.html',context)
+
+def about(request):
+	# return HttpResponse('<h1>Blog About</h1>')
+	return render(request,'blog/about.html',{'title':'About'})
